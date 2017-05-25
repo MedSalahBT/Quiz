81a4bb3d59d69d7d894a58a1f26dc4900955a47d
MedSalahBT <bentaaritmohamedsalah@gmail.com>
2017-05-25 13:43:37 +0200

    Last commit

--- ConnexionDB.php
+++ ConnexionDB.php
@@ -1,25 +1,6 @@
 <?php
-	define('DB_HOST', 'localhost');  
-	define('DB_USER', 'root');
-	define('DB_PASSWORD','');
-	define('DB_NAME', 'quiz');
-
-
-$mysqli = new mysqli("localhost", "med", "medbt", "quiz");
-if ($mysqli->connect_errno) {
-    echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-	// $dblink = mysql_connect('localhost', 'med', 'medbt');
-
-	// if($dblink) {
- //    	$selectdatabase = mysql_select_db('quiz');
- //    if(!$selectdatabase) {
- //        echo 'erreur lors de la selection de la bdd';
- //        exit;
- //    }
-	// } else {
- //    	echo 'erreur de connexion a la bdd';
- //    	exit;
-	// }
-
+	$mysqli = new mysqli("localhost", "med", "medbt", "quiz");
+	if ($mysqli->connect_errno) {
+	    echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
 ?>
